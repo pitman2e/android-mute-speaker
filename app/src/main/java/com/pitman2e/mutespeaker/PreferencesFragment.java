67b81ee3f70c0fb35f763f88896346bfda3e735f
pitman2e <pitman2e@gmail.com>
2020-01-11 14:48:22 +0800

    Fixed when Service Toggle in notification does not reflect in activity

--- app/src/main/java/com/pitman2e/mutespeaker/PreferencesFragment.java
+++ app/src/main/java/com/pitman2e/mutespeaker/PreferencesFragment.java
@@ -3,8 +3,10 @@ package com.pitman2e.mutespeaker;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
+
+import static com.pitman2e.mutespeaker.Prefs.getIsEnableNotification;
 
 public class PreferencesFragment  extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
@@ -14,7 +16,14 @@ public class PreferencesFragment  extends PreferenceFragmentCompat implements Sh
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         MuteServiceToggle.EnforceByPref(this.getActivity());
+
+        if (getString(R.string.PREFERENCES_ID_ENABLE_MUTE_SERVICE).equals(key)) {
+            SwitchPreference switchPreference = findPreference(key);
+            if (switchPreference != null) {
+                switchPreference.setChecked(getIsEnableNotification(this.getActivity()));
+            }
+        }
     }
 }
