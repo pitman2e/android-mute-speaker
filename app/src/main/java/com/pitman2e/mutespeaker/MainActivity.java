ae27b312fb9eaba13517900d95c66a4fbd0848b4
pitman2e <pitman2e@gmail.com>
2020-01-11 14:29:41 +0800

    Move code from Activity to Fragment

--- app/src/main/java/com/pitman2e/mutespeaker/MainActivity.java
+++ app/src/main/java/com/pitman2e/mutespeaker/MainActivity.java
@@ -1,21 +1,13 @@
 package com.pitman2e.mutespeaker;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-        MuteServiceToggle.EnforceByPref(this);
     }
 }
--- app/src/main/java/com/pitman2e/mutespeaker/PreferencesFragment.java
+++ app/src/main/java/com/pitman2e/mutespeaker/PreferencesFragment.java
@@ -1,11 +1,20 @@
 package com.pitman2e.mutespeaker;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
 import androidx.preference.PreferenceFragmentCompat;
 
-public class PreferencesFragment  extends PreferenceFragmentCompat {
+public class PreferencesFragment  extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.fragmemt_preferences, rootKey);
+        PreferenceManager.getDefaultSharedPreferences(this.getActivity()).registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        MuteServiceToggle.EnforceByPref(this.getActivity());
     }
 }
