4e5d7cc72d816b37eae5c77ef3f0099d509b5916
pitman2e <pitman2e@gmail.com>
2021-02-27 16:56:23 +0800

    Remove SDK if-else block

--- app/src/main/java/com/pitman2e/mutespeaker/VolumeSettingContentObserver.java
+++ app/src/main/java/com/pitman2e/mutespeaker/VolumeSettingContentObserver.java
@@ -47,18 +47,14 @@ public class VolumeSettingContentObserver extends ContentObserver {
             return false;
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            AudioDeviceInfo[] audioDevices = audioManager.getDevices(AudioManager.GET_DEVICES_ALL);
-            for (AudioDeviceInfo deviceInfo : audioDevices) {
-                if (deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADPHONES
-                        || deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADSET
-                ) {
-                    return true;
-                }
+        AudioDeviceInfo[] audioDevices = audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS);
+        for (AudioDeviceInfo deviceInfo : audioDevices) {
+            if (deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADPHONES
+                    || deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADSET
+            ) {
+                return true;
             }
-            return false;
-        } else {
-            return audioManager.isWiredHeadsetOn();
         }
+        return false;
     }
 }
