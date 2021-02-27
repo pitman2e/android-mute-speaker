143f3e2337451a8630402c51b4ef9dc1ee5bebea
pitman2e <pitman2e@gmail.com>
2021-02-27 17:04:33 +0800

    Add new headphone types

--- app/src/main/java/com/pitman2e/mutespeaker/VolumeSettingContentObserver.java
+++ app/src/main/java/com/pitman2e/mutespeaker/VolumeSettingContentObserver.java
@@ -49,8 +49,14 @@ public class VolumeSettingContentObserver extends ContentObserver {
 
         AudioDeviceInfo[] audioDevices = audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS);
         for (AudioDeviceInfo deviceInfo : audioDevices) {
+            //Device types:
+            //https://developer.android.com/reference/android/media/AudioDeviceInfo#summary
             if (deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADPHONES
+                    || deviceInfo.getType() == AudioDeviceInfo.TYPE_USB_HEADSET
                     || deviceInfo.getType() == AudioDeviceInfo.TYPE_WIRED_HEADSET
+                    || deviceInfo.getType() == AudioDeviceInfo.TYPE_BLUETOOTH_SCO
+                    || deviceInfo.getType() == AudioDeviceInfo.TYPE_BLUETOOTH_A2DP
+                    || deviceInfo.getType() == AudioDeviceInfo.TYPE_AUX_LINE
             ) {
                 return true;
             }
