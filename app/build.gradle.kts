3663e8b7be404fc5faedb68a2e0eefc0427f3f26
pitman2e <pitman2e@gmail.com>
2023-09-26 19:55:50 +0800

    Remove duplicated lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,6 @@ dependencies {
     implementation ("com.google.android.material:material:1.9.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation ("androidx.preference:preference-ktx:1.2.1")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
