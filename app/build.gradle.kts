655317abe2a38bc6097a5a17dac79851f4230ddc
pitman2e <pitman2e@gmail.com>
2024-09-16 20:30:35 +0800

    Pop version to 20240916.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "20231014.1"
+        versionName = "20240916.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
