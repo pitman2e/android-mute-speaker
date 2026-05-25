75518e77907effa1309c3c381d28ba44bca46656
pitman2e <pitman2e@gmail.com>
2026-05-25 17:52:21 +0800

    Added Jetpack Work deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,4 +77,8 @@ dependencies {
     // Firebase SDK for Google Analytics
     implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
     implementation("com.google.firebase:firebase-analytics")
+
+    // Jetpack WorkManager
+    val workVersion = "2.11.2"
+    implementation("androidx.work:work-runtime-ktx:${workVersion}")
 }
