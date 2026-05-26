66c77635f9052983461a458765b7c5e184099d59
pitman2e <pitman2e@gmail.com>
2026-05-26 22:01:23 +0800

    Handle service restart with intent == null

--- app/src/main/java/com/github/pitman2e/mutespeaker/services/HeadsetStateService.kt
+++ app/src/main/java/com/github/pitman2e/mutespeaker/services/HeadsetStateService.kt
@@ -9,6 +9,7 @@ import android.content.IntentFilter
 import android.os.Build
 import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.provider.Settings
 import androidx.core.app.NotificationCompat
 import com.github.pitman2e.mutespeaker.R
@@ -50,8 +51,12 @@ class HeadsetStateService : Service() {
         super.onCreate()
     }
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         createEnableMuteSpeakerNotification()
+        if (intent == null) {
+            return START_STICKY
+        }
+
         return super.onStartCommand(intent, flags, startId)
     }
 
